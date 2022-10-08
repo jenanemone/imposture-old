d472a6a8ede467193bb0e8a6d8c44eab970e5dce
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2022-10-07 21:42:29 -0400

    Update code for TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./views/**/*.ejs"],
-
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
+  theme: {
+    extend:{
+      colors:{
           "primary": "#FF5D73",
           "secondary": "#494949",
           "accent": "#7C7A7A",
           "neutral": "#000000",
           "base-100": "#F1EBEB"
-        }
       }
-    ]
+    },
+  },
+
+  plugins: [require("daisyui")],
+  daisyui: {
+   
   }
 }
